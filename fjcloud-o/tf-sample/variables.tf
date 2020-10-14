8dfabc601f8ddefab493cea1f47c6a791de4155f
tyamac <tyamac@Enterpise.local>
2020-10-14 17:39:01 +0900

    注釈を追加

--- fjcloud-o/tf-sample/variables.tf
+++ fjcloud-o/tf-sample/variables.tf
@@ -1,3 +1,4 @@
+# provider
 variable auth_url {}
 variable tenant_name {}
 variable tenant_id {}
@@ -6,6 +7,8 @@ variable project_domain_id {}
 variable region {}
 variable user_name {}
 variable password {}
+
+# network
 variable network_name {}
 variable subnet_name {}
 variable cidr {}
